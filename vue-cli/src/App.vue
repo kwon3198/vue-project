4b14fb7b386318c1660eb9ed2591dbe6f58b3682
kwon3198 <dkssk4468@naver.com>
2021-04-29 17:46:31 +0900

    props

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return {
+      str: 'hi'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
